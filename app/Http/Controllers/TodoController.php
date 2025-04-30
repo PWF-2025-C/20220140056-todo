a0f6e69e15b114a8efbe4b89a11d5ce0f8e0d560
vinarahmavionita <vinarahmavionita824@gmail.com>
2025-04-30 18:20:54 +0700

    update, delete data

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -10,8 +10,12 @@ class TodoController extends Controller
     public function index()
     {
         $todos = Todo::where('user_id', Auth::id())->orderBy('is_done', 'desc')->get();
- 
-         return view('todo.index', compact('todos'));
+
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_done', true)
+            ->count();
+
+        return view('todo.index', compact('todos', 'todosCompleted'));
     }
 
     public function create()
@@ -20,9 +24,36 @@ class TodoController extends Controller
     }
 
 
-    public function edit()
+    //Edit Data – Todo
+    public function edit(Todo $todo)
     {
-        return view('todo.edit');
+        if (auth()->user()->id == $todo->user_id) {
+            // dd($todo);
+            return view('todo.edit', compact('todo'));
+        } else {
+            // abort(403);
+            // abort(403, 'Not authorized');
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to edit this todo!');
+        }
+
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        // Practical
+        // $todo->title = $request->title;
+        // $todo->save();
+
+        // Eloquent Way - Readable
+        $todo->update([
+            'title' => ucfirst($request->title),
+        ]);
+
+        return redirect()->route('todo.index')->with('success', 'Todo updated successfully!');
     }
 
     public function store(Request $request)
@@ -36,5 +67,56 @@ class TodoController extends Controller
             'user_id' => Auth::id(),
         ]);
         return redirect()->route('todo.index')->with('success', 'Todo Created Successfully');
+    }
+
+    public function complete(Todo $todo)
+    {
+        if ($todo->user_id != Auth::id()) {
+            abort(403);
+        }
+
+        $todo->is_done = true;
+        $todo->save();
+
+        return redirect()->route('todo.index')->with('success', 'Todo marked as complete.');
+    }
+
+    public function uncomplete(Todo $todo)
+    {
+
+        if ($todo->user_id != Auth::id()) {
+            abort(403);
+        }
+
+        $todo->is_done = false;
+        $todo->save();
+
+        return redirect()->route('todo.index')->with('success', 'Todo marked as uncomplete.');
+    }
+
+    //delete data - todo
+    public function destroy(Todo $todo)
+    {
+        if (auth()->user()->id == $todo->user_id) {
+            $todo->delete();
+            return redirect()->route('todo.index')->with('success', 'Todo deleted successfully!');
+        } else {
+            return redirect()->route('todo.index')->with('danger', 'You are not authorized to delete this todo!');
+        }
+    }
+
+    public function destroyCompleted()
+    {
+        // get all todos for current user where is_completed is true
+        $todosCompleted = Todo::where('user_id', auth()->user()->id)
+            ->where('is_done', true)
+            ->get();
+
+        foreach ($todosCompleted as $todo) {
+            $todo->delete();
+        }
+
+        // dd($todosCompleted);
+        return redirect()->route('todo.index')->with('success', 'All completed todos deleted successfully!');
     }
 }
---
+++ screenshot/tugas6.md
@@ -0,0 +1,16 @@
+1. Todo Completed
+![alt text](<Screenshot 2025-04-30 173002.png>)
+2. Todo Uncompleted
+![alt text](<Screenshot 2025-04-30 173026.png>)
+3. Todo Edited
+![alt text](<Screenshot 2025-04-30 175803.png>)
+4. Todo Deleted
+![alt text](<Screenshot 2025-04-30 181409.png>)
+5. Todo Deleted All Completed Todo
+![alt text](<Screenshot 2025-04-30 181433.png>)
+6. User Make Admin
+![alt text](<Screenshot 2025-04-30 174354.png>)
+7. User Remove Admin
+![alt text](<Screenshot 2025-04-30 174641.png>)
+8. User Deleted 
+![alt text](<Screenshot 2025-04-30 182001.png>)
